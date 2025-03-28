5bb255ee42293e726534a82f7ddd2aa21f772ca5
Shreyash <shreyash.j@kgs.live>
2025-03-28 16:09:17 +0530

    first program

--- Patterns/Program1.cpp
+++ Patterns/Program1.cpp
@@ -1,1 +1,18 @@
-hi
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern(int n){
+    for(int row=1; row<=n;row++){
+        for(int col=1; col<=row;col++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+int main() {
+    cout << "Hello, Manjaro!" << endl;
+    pattern(4);
+    return 0;
+}
